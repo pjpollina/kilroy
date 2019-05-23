9fe5fe24a9916531a696466758cb462a863773f5
PJ Pollina <contact@pjpollina.info>
2019-05-23 17:16:22 -0400

    Added mysql.rb
    Stolen from my site project

---
+++ mysql.rb
@@ -0,0 +1,23 @@
+# Functional programming-centric interface for MySQL database
+
+require 'mysql2'
+
+class MySQL
+  def initialize(username, password, database)
+    @username = username
+    @password = password
+    @database = database
+  end
+
+  def connect
+    client = new_client
+    yield(client)
+    client.close
+  end
+
+  private
+
+  def new_client
+    Mysql2::Client.new(username: @username, password: @password, database: @database)
+  end
+end
