a9a6be50f31f2c6f7ed109eb2b3b2633ff5fe6e1
PJ Pollina <contact@pjpollina.info>
2020-11-03 13:48:55 -0500

    Trimmed #status significantly

--- kilroy.rb
+++ kilroy.rb
@@ -58,60 +58,54 @@ kilroy.message(in: '#weigh-ins') do |event|
 end
 
 kilroy.message(in: '#status') do |event|
-  totals_sql = 'SELECT cd_mph, SUM(cd_minutes) AS minutes, SUM(cd_distance) AS distance FROM cardio WHERE '
-  hills_sql  = 'SELECT cd_incline, SUM(cd_minutes) AS minutes, SUM(cd_distance) AS distance FROM cardio WHERE cd_mph=4.0 AND '
+  statements = {
+    "~totals" => [
+      'SELECT cd_mph, SUM(cd_minutes) AS minutes, SUM(cd_distance) AS distance FROM cardio WHERE ',
+      'mph'
+    ],
+    "~hills" => [
+      'SELECT cd_incline, SUM(cd_minutes) AS minutes, SUM(cd_distance) AS distance FROM cardio WHERE cd_mph=4.0 AND ',
+      'incline'
+    ]
+  }
   span_constraints = {
     'month'    => 'MONTH(cd_date)=? AND YEAR(cd_date)=?',
     'semester' => 'MONTH(cd_date) BETWEEN ? AND ? AND YEAR(cd_date)=?',
     'year'     => 'YEAR(cd_date)=?'
   }
 
-  case event.content
-  when /\A~totals (.*)/
-    command = event.content.split(' ')
-    if(command.count > 1 && span_constraints.keys.include?(command[1]))
-      args = []
-      case command[1]
-        when 'month'    then args = [Time.now.month, Time.now.year]
-        when 'semester' then args = ((Time.now.month.between?(1, 6)) ? [1, 6] : [7, 12]) << Time.now.year
-        when 'year'     then args = [Time.now.year]
-      end
-      message = "```#{command[1].capitalize} totals:\r\n"
-      mysql.connect do |client|
-        all = Hash.new(0)
-        stmt = client.prepare("#{totals_sql} #{span_constraints[command[1]]} GROUP BY cd_mph")
-        stmt.execute(*args, symbolize_keys: true).each do |total|
+  unless(event.content.match?(/\A~totals (.*)/) || event.content.match?(/\A~hills (.*)/))
+    event.respond "Unknown command #{event.content}"
+    next
+  end
+
+  command = event.content.split(' ')
+  statement = "#{statements[command[0]][0]} #{span_constraints[command[1]]} GROUP BY cd_#{statements[command[0]][1]}"
+  if(command.count > 1 && span_constraints.keys.include?(command[1]))
+    args = []
+    case command[1]
+      when 'month'    then args = [Time.now.month, Time.now.year]
+      when 'semester' then args = ((Time.now.month.between?(1, 6)) ? [1, 6] : [7, 12]) << Time.now.year
+      when 'year'     then args = [Time.now.year]
+    end
+    message = "```#{command[1].capitalize} #{command[0][1..-1]}:\r\n"
+    mysql.connect do |client|
+      all = Hash.new(0)
+      stmt = client.prepare(statement)
+      stmt.execute(*args, symbolize_keys: true).each do |total|
+        if(command[0] == '~totals')
           message << "#{total[:cd_mph].to_s.ljust(4)}\t#{total[:minutes].to_i.to_s.rjust(5)}\t#{("%.3f" % total[:distance].round(3)).rjust(7)}\r\n"
           total.keys.each {|key| all[key] += total[key]}
-        end
-        message << "ALL \t#{all[:minutes].to_i.to_s.rjust(5)}\t#{("%.3f" % all[:distance].round(3)).rjust(7)}\r\n"
-      end
-      event.respond(message + '```')
-    else
-      event.respond('Missing or unrecognized qualifier for command "totals"')
-    end
-  when /\A~hills (.*)/
-    command = event.content.split(' ')
-    if(command.count > 1 && span_constraints.keys.include?(command[1]))
-      args = []
-      case command[1]
-        when 'month'    then args = [Time.now.month, Time.now.year]
-        when 'semester' then args = ((Time.now.month.between?(1, 6)) ? [1, 6] : [7, 12]) << Time.now.year
-        when 'year'     then args = [Time.now.year]
-      end
-      message = "```#{command[1].capitalize} hills:\r\n"
-      mysql.connect do |client|
-        stmt = client.prepare("#{hills_sql} #{span_constraints[command[1]]} GROUP BY cd_incline")
-        stmt.execute(*args, symbolize_keys: true).each do |total|
+        else
           message << "#{total[:cd_incline].to_s.rjust(4)}\t#{total[:minutes].to_i.to_s.rjust(4)}\t#{("%.3f" % total[:distance].round(3)).rjust(7)}\r\n"
         end
-        event.respond(message + "```")
       end
-    else
-      event.respond('Missing or unrecognized qualifier for command "hills"')
+      message << "ALL \t#{all[:minutes].to_i.to_s.rjust(5)}\t#{("%.3f" % all[:distance].round(3)).rjust(7)}\r\n" unless all.empty?
     end
+    event.respond(message + '```')
+  else
+    event.respond("Missing or unrecognized qualifier for command \"#{command[0][1..-1]}\"")
   end
-  puts "Command issued: #{event.content}"
 end
 
 kilroy.run
