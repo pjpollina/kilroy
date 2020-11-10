7bb48bbb1b1da97044cb858469eea109d9a801e9
PJ Pollina <contact@pjpollina.info>
2020-11-10 13:23:44 -0500

    Added import for lib/status

--- kilroy.rb
+++ kilroy.rb
@@ -4,6 +4,7 @@ require 'dotenv/load'
 require 'discordrb'
 require 'lib/mysql'
 require 'lib/backup'
+require 'lib/status'
 
 REG_RUN  = /\A(?:1)?[0-9]\.[0-9], [0-9]{1,2}m\z/
 REG_HILL = /\A[1-9]\.[0-9], [0-9]{1,2}m, [1-9]\.[0-9]%\z/
