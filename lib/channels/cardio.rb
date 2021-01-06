c88e5a65ccb37ff7655815db5e9197d47e0861a0
PJ Pollina <contact@pjpollina.info>
2021-01-06 16:58:41 -0500

    Added comments to Cardio

--- lib/channels/cardio.rb
+++ lib/channels/cardio.rb
@@ -3,16 +3,20 @@
 module Cardio
   extend self
 
+  # Regexps representing valid cardio records
   REG_RUN  = /\A([1-9]|[1-9][0-9])\.(0|5), ([1-9]|[1-5][0-9]|60)m\z/
   REG_HILL = /#{REG_RUN.source.chomp('\z')}, ([1-9]|[1-9][0-9])\.(0|5)%\z/
 
+  # SQL statements for inserting cardio records
   INSERT_RUN  = 'INSERT INTO cardio(cd_date, cd_mph, cd_minutes) VALUES(CURDATE(), ?, ?)'
   INSERT_HILL = 'INSERT INTO cardio(cd_date, cd_mph, cd_minutes, cd_incline) VALUES(CURDATE(), ?, ?, ?)'
 
+  # Returns whether or not a given command is a valid cardio record
   def valid?(command)
     /#{REG_RUN.source.chomp('\z')}(?:, [1-9]\.[0-9]%)?/.match?(command)
   end
 
+  # Returns an array of SQL args parsed from the given command
   def argparse(command)
     mph, minutes, incline = command.split(', ')
     args = [mph.to_f, minutes.chomp('m').to_i]
@@ -20,6 +24,7 @@ module Cardio
     return args
   end
 
+  # Returns the appropriate SQL statement for the given command
   def statement(command)
     case command
       when REG_RUN  then return INSERT_RUN
@@ -28,6 +33,7 @@ module Cardio
     end
   end
 
+  # Inserts the cardio record for the given command using mysql and returns a status message
   def insert(command, mysql)
     return "Unrecognized cardio format", true unless valid?(command)
     mysql.execute(statement(command), argparse(command))
