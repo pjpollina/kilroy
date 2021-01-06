a6ac1a2f696326ea7d39a6bd65f4f8a94ff3063d
PJ Pollina <contact@pjpollina.info>
2021-01-06 17:18:22 -0500

    Added comments to MySQL

--- lib/clients/mysql.rb
+++ lib/clients/mysql.rb
@@ -3,6 +3,7 @@
 require 'mysql2'
 
 class MySQL
+  # Creates a new MySQL object, defaulting to the intended parameters
   def initialize(username="kilroy", password=ENV['discord_bot_token'], database="fitness", host=ENV['sql_host'])
     @username = username
     @password = password
@@ -10,12 +11,14 @@ class MySQL
     @host     = host || "localhost"
   end
 
+  # Connects to MySQL and yields the client object, closing it afterwards
   def connect
     client = new_client
     yield(client)
     client.close
   end
 
+  # Prepares and executes the given statement with args, yields the results, then closes it
   def execute(statement, args=[])
     connect do |client|
       stmt = client.prepare(statement)
@@ -27,6 +30,7 @@ class MySQL
 
   private
 
+  # Creates a new Mysql2 client using this object's attributes
   def new_client
     Mysql2::Client.new(username: @username, password: @password, database: @database, host: @host)
   end
